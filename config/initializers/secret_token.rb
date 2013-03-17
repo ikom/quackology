374526f0e32505e73a787a322998559b714e77ee
Ivan Komkov <ivankomkov@gmail.com>
2013-03-17 11:44:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quackology::Application.config.secret_token = 'b8e4bbf7d61d51314794cde168dc6c330f3cec8ce050cc1ec7261052a25c43ebd3c7a7ad563857b67523e5df0ad8b4ef595be1676cce7e5defee5a40ce187ce1'
